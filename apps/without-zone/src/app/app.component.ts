c2e4b5fbab4c9bccb9e4d2d690e1ca7da8e67df6
r.vinokurov <r.vinokurov@tinkoff.ru>
2023-03-13 22:59:02 +0300

    add app for signals

---
+++ apps/without-zone/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import {RouterModule} from '@angular/router';
+import {ChangeDetectionStrategy, Component, NgZone} from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule],
+  selector: 'wz-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'change-detection-demo';
+
+  protected logOptions() {
+    return [
+      `color: white`,
+      `background: gray`,
+      `padding: 2px 10px 2px 10px`,
+      `margin: 20px 10px`,
+      'font-weight: 600',
+      'border-radius: 3px',
+    ];
+  }
+
+  protected log(message: string) {
+    console.log(`%c-> ${message}`, this.logOptions().join(';'));
+  }
+
+  constructor(private readonly ngZone: NgZone) {
+    this.ngZone.onStable.subscribe(() => {
+      this.log('Zone stable');
+    });
+
+    this.ngZone.onUnstable.subscribe(() => {
+      this.log('Zone unstable');
+    });
+  }
+}
